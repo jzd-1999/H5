abb6d3c251be3b338f58d45e37243e788d643984
JZD <1771887904@qq.com>
2021-10-17 11:53:56 +0800

    放大镜有bug

--- js/01.js
+++ js/01.js
@@ -1,10 +1,13 @@
 window.onload = function () {
     const box = document.querySelector('#box')
-    let x = null,
+    
+    box.addEventListener('mousemove', function (e) {
+        let x = null,
         y = null,
         maskX = null,
-        maskY = null;
-    box.addEventListener('mousemove', function (e) {
+        maskY = null,
+        imgX = null,
+        imgY = null;
         x = e.pageX - box.offsetLeft;
         y = e.pageY - box.offsetTop;
         box.children[0].style.left = x - box.children[0].offsetWidth / 2 + 'px'
@@ -14,16 +17,22 @@ window.onload = function () {
         box.children[0].style.display = 'block';
         box.children[2].style.display = 'block';
         if (maskX <= 0) {
-            box.children[0].style.left = 0 
-        }else if (maskX >= box.offsetWidth - box.children[0].offsetWidth) {
+            box.children[0].style.left = 0
+        } else if (maskX >= box.offsetWidth - box.children[0].offsetWidth) {
             box.children[0].style.left = box.offsetWidth - box.children[0].offsetWidth + 'px'
         }
         if (maskY <= 0) {
-            box.children[0].style.top = 0 
-        }else if (maskY >= box.offsetHeight - box.children[0].offsetHeight) {
+            box.children[0].style.top = 0
+        } else if (maskY >= box.offsetHeight - box.children[0].offsetHeight) {
             box.children[0].style.top = box.offsetHeight - box.children[0].offsetHeight + 'px'
         }
         box.children[2].children[0].src = box.children[1].src
+        // let imgMaxX = box.children[2].children[0].
+        imgX = -maskX 
+        imgY = -maskY 
+        box.children[2].children[0].style.left = imgX + 'px'
+        box.children[2].children[0].style.top = imgY + 'px'
+
     })
     box.addEventListener('mouseleave', function (e) {
         box.children[0].style.display = 'none';
